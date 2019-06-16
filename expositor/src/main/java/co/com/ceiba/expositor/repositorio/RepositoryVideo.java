6fb36b515cd5e8a2d67daedb20112da41d790b4d
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-06-16 18:13:15 -0500

    Crear el repositorio dede Video

---
+++ expositor/src/main/java/co/com/ceiba/expositor/repositorio/RepositoryVideo.java
@@ -0,0 +1,9 @@
+package co.com.ceiba.expositor.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.com.ceiba.expositor.entity.Video;
+
+public interface RepositoryVideo extends JpaRepository<Video, Integer>{
+
+}
