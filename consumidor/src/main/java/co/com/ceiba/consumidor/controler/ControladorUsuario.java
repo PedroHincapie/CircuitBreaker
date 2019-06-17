46b357e32339defe253b7090221cb5ed0ce10700
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-06-17 13:47:25 -0500

    Añadir controler par el consumo de los servicios de usuario

--- consumidor/src/main/java/co/com/ceiba/consumidor/controler/ControladorUsuario.java
+++ consumidor/src/main/java/co/com/ceiba/consumidor/controler/ControladorUsuario.java
@@ -1,23 +1,33 @@
 package co.com.ceiba.consumidor.controler;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.com.ceiba.consumidor.clienteweb.ClienteUsuarioParaVideo;
 import co.com.ceiba.consumidor.repository.RepositorioUsuario;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(path="/usuario")
 public class ControladorUsuario {
 
 	private RepositorioUsuario repositorioUsuario;
+	private ClienteUsuarioParaVideo clienteUsuarioParaVideo;
 
-	public ControladorUsuario(RepositorioUsuario repositorioUsuario) {
-		this.repositorioUsuario = repositorioUsuario;		
+	public ControladorUsuario(RepositorioUsuario repositorioUsuario, ClienteUsuarioParaVideo clienteUsuarioParaVideo) {
+		this.repositorioUsuario = repositorioUsuario;
+		this.clienteUsuarioParaVideo = clienteUsuarioParaVideo;
 	}
 
 	@GetMapping
 	public  long consultaUsuario() {
 		return repositorioUsuario.count();
+	}
+
+	@GetMapping(path="/{id}")
+	public Mono<Integer> consultaCantidadDeVideosPorUsuario(@PathVariable Integer id) {
+		return clienteUsuarioParaVideo.consultarVideos(id);
 	}
 }
