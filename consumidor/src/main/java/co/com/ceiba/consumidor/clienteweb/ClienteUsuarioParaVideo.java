d78fe128f0073e9d504083420ec9fb50026d0917
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-06-17 13:46:49 -0500

    Crear Cliente para el consumo de los servicos externos

---
+++ consumidor/src/main/java/co/com/ceiba/consumidor/clienteweb/ClienteUsuarioParaVideo.java
@@ -0,0 +1,21 @@
+package co.com.ceiba.consumidor.clienteweb;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class ClienteUsuarioParaVideo {
+
+	private WebClient cliente = WebClient.create("http://localhost:8083");
+
+	public  Mono<Integer> consultarVideos(Integer idUsuario) {
+		return cliente.get()
+				.uri("/video/{id}", idUsuario)
+				.retrieve()
+				.bodyToMono(Integer.class);
+	}
+
+}
