0f5f7f454928185885584dd7940c1f6c55a29ad0
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-06-17 14:51:13 -0500

    Crear servico de consulta videos por id

--- expositor/src/main/java/co/com/ceiba/expositor/controlador/ControladorVideo.java
+++ expositor/src/main/java/co/com/ceiba/expositor/controlador/ControladorVideo.java
@@ -1,13 +1,18 @@
 package co.com.ceiba.expositor.controlador;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.com.ceiba.expositor.comando.ComandoVideo;
+import co.com.ceiba.expositor.entity.Video;
 import co.com.ceiba.expositor.repositorio.RepositoryVideo;
 
 @RestController
-@RequestMapping(path= "/video")
+@RequestMapping(path = "/video")
 public class ControladorVideo {
 
 	private RepositoryVideo repositoryVideo;
@@ -17,9 +22,19 @@ public class ControladorVideo {
 	}
 
 	@GetMapping
-	public  long consultarVideos() {
+	public long consultarVideos() {
 		return repositoryVideo.count();
 	}
 
+	@GetMapping(path = "/{id}")
+	public Integer consultarVideosPorId(@PathVariable Integer id) {
+		return repositoryVideo.cantidadVideoPorId(id);
+	}
 
+	@PostMapping
+	public void crearVideo(@RequestBody ComandoVideo comandoVideo) {
+		Video entity = new Video(comandoVideo.getNombrePelicula(), comandoVideo.getIdUsuario());
+		entity = repositoryVideo.save(entity);
+		System.err.println(entity.getId());
+	}
 }
