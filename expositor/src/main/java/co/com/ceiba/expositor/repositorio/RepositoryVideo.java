b58d21cc3f505d998dde84a2ab5c89ab661ed1a6
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-06-17 13:45:27 -0500

    Crear Query que cuenta los video por usuario

--- expositor/src/main/java/co/com/ceiba/expositor/repositorio/RepositoryVideo.java
+++ expositor/src/main/java/co/com/ceiba/expositor/repositorio/RepositoryVideo.java
@@ -1,9 +1,14 @@
 package co.com.ceiba.expositor.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import co.com.ceiba.expositor.entity.Video;
 
+@Repository
 public interface RepositoryVideo extends JpaRepository<Video, Integer>{
 
+	@Query(value = "select COUNT(*) FROM video v where v.idUsuario = ?1", nativeQuery = true)
+	Integer cantidadVideoPorId(Integer idUsuario);
 }
